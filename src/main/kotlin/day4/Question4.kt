b72ac45f10692a192e028aa20e31ae61a61cf24d
Yash-Garg <yash.garg@outlook.in>
2022-12-20 00:30:10 +0530

    add day4 part1 solution

--- src/main/kotlin/day4/Question4.kt
+++ src/main/kotlin/day4/Question4.kt
@@ -6,11 +6,32 @@ class Question4(override val day: String) : Question<Int> {
 
     private val inputList = getInput()
 
+    private val String.pair: Pair<Int, Int>
+        get() {
+            val groups = split("-")
+            return Pair(groups.first().toInt(), groups.last().toInt())
+        }
+
+    private fun Pair<Int, Int>.containsFully(pair: Pair<Int, Int>): Boolean {
+        return (pair.first in first..second) && (pair.second in first..second)
+    }
+
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        var numPairs = 0
+
+        inputList.forEach {
+            val pairs = it.split(",")
+
+            val p1 = pairs.first().pair
+            val p2 = pairs.last().pair
+
+            if (p1.containsFully(p2) || p2.containsFully(p1)) numPairs++
+        }
+
+        return numPairs
     }
 
     override fun part2(): Int {
-        TODO("Not yet implemented")
+        return 0
     }
 }
